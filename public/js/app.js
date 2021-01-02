1ef8d0f3c5b64bd410253b168de3f6c40501eae5
jonathangani279 <jonathangani279@gmail.com>
2021-01-02 13:14:21 +0700

    merge

--- public/js/app.js
+++ public/js/app.js
@@ -78305,7 +78305,7 @@ var axios_baseURL = {
 
 window.axios = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-window.axios.defaults.baseURL = axios_baseURL.jonathan;
+window.axios.defaults.baseURL = "https://103.214.113.153/api/v1/";
 /**
  * Next we will register the CSRF Token as a common header with Axios so that
  * all outgoing HTTP requests automatically have it attached. This is just
